11d047c9c9500b89d869e673aa34a250ab74eec3
Itallo Augusto Pereira de Azevedo Melo <iapam@WINDOWS.CIN.UFPE.BR>
2024-05-23 16:43:50 -0300

    criação da API Banco de Dados

--- src/Components/Form/Select.js
+++ src/Components/Form/Select.js
@@ -1,11 +1,14 @@
 import styles from './Select.module.css'
 
-function Select({ text, name, optons, handleOnChange, value }) {
+function Select({ text, name, options, handleOnChange, value }) {
     return (
         <div className={styles.form_control}>
         <label htmlFor={name}>{text}:</label>
         <select name={name} id={name}>
             <option>Selecione uma opção</option>
+            {options.map((option) => (
+                <option value={option.id} key={option.id}>{option.name}</option>
+            ))}
         </select>
     </div>
     )
--- src/Components/Project/ProjectForm.js
+++ src/Components/Project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Input from '../Form/Input';
 import Select from '../Form/Select';
 import SubmitButton from '../Form/SubmitButton';
@@ -5,6 +7,23 @@ import SubmitButton from '../Form/SubmitButton';
 import styles from './ProjectForm.module.css';
 
 function ProjectForm({ btnText }) {
+    const [categories, setCategories] = useState([])
+
+    // Hook - useEffect
+    useEffect(() => {
+        // fecth - para pega as categorias do DB
+            fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+        
     return (
         <form className={styles.form}>
             <Input 
@@ -19,7 +38,12 @@ function ProjectForm({ btnText }) {
                 name="budget"
                 placeholder="Insira o orçamento do projeto"
             />
-            <Select name="category_id" text="Selecione a categoria"/>
+            <Select 
+            // esse select é para que consiga renderiza na pág criar projeto como opções de categorias
+                name="category_id" 
+                text="Selecione a categoria" 
+                options={categories}
+            />
             <SubmitButton text={btnText}/>
         </form>
     )
